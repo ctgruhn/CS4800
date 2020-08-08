f892a51641fc813d909d0aa39327b5fea55b3770
Carl Gruhn <cgruhn@bgsu.edu>
2020-08-08 12:07:25 -0400

    Init TODOs

--- project2.py
+++ project2.py
@@ -5,11 +5,36 @@ from nltk.tokenize import word_tokenize
 stop_words = set(stopwords.words("english"))
 
 def tokenize():
-    words = []
-    for word in reuters.words():
-        word_tokenize(word.lower())
-        if word not in stop_words:
-            words.append(word)
-    return words
+    # words = []
+    # for word in reuters.words():
+    #     word_tokenize(word.lower())
+    #     if word not in stop_words:
+    #         words.append(word)
+    # return words
 
-print(tokenize()) #For testing purposes
+    """
+    The above code did nothing, reuters.words() is already tokenized.
+    Replace w/ text from reuters docs.
+    """
+    pass
+
+
+# TODO: Separate Documents
+print(reuters.fileids())
+#Test
+documents = {}
+for docIDs in reuters.fileids():
+    documents[docIDs] = reuters.raw(docIDs)
+
+print(documents)
+# TODO: Tokenize Words
+# TODO: Term freq w/in each doc
+# TODO: Doc freq for each term
+# TODO: TF-IDF Calc
+# TODO: One more weight system
+
+# TODO: Analyze Queries
+# TODO: Calculate Precision 
+# TODO:           & Recall
+# TODO: Graph Precision & Recall
+
