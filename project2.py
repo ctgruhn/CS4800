e2270883f9f73650a090d43117b7880be013889a
Carl Gruhn <cgruhn@bgsu.edu>
2020-08-07 20:09:39 -0400

    Init tokenize function

---
+++ project2.py
@@ -0,0 +1,15 @@
+import nltk
+from nltk.corpus import stopwords, reuters
+from nltk.tokenize import word_tokenize
+
+stop_words = set(stopwords.words("english"))
+
+def tokenize():
+    words = []
+    for word in reuters.words():
+        word.lower()
+        if word not in stop_words:
+            words.append(word)
+    return words
+
+print(tokenize()) #For testing purposes
