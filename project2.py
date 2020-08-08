779b710360faedf014038af73841ad4e2b5b2473
Carl Gruhn <cgruhn@bgsu.edu>
2020-08-08 17:25:03 -0400

    Update tokenizing can set term frequency

--- project2.py
+++ project2.py
@@ -1,40 +1,53 @@
 import nltk
 from nltk.corpus import stopwords, reuters
 from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.stem import WordNetLemmatizer
 
 stop_words = set(stopwords.words("english"))
 
-def tokenize():
-    # words = []
-    # for word in reuters.words():
-    #     word_tokenize(word.lower())
-    #     if word not in stop_words:
-    #         words.append(word)
-    # return words
-
-    """
-    The above code did nothing, reuters.words() is already tokenized.
-    Replace w/ text from reuters docs.
-    """
-    pass
-
+# TODO: Tokenize Words
+def tokenize(document):
+    words = []
+    tokens = word_tokenize(document)
+    lemmatizer = WordNetLemmatizer()
+    for word in tokens:
+        if word not in stop_words and word.isalpha():
+            words.append(lemmatizer.lemmatize(word.lower()))
+    return words
 
 # TODO: Separate Documents
-print(reuters.fileids())
+# print(reuters.fileids())
 #Test
-documents = {}
-for docIDs in reuters.fileids():
-    documents[docIDs] = reuters.raw(docIDs)
+def get_docs():
+    documents = {}
+    for docID in reuters.fileids():
+        words = reuters.raw(docID)
+        documents[docID] = tokenize(words)
+    return documents
 
-print(documents)
-# TODO: Tokenize Words
 # TODO: Term freq w/in each doc
+def term_frequency(text):
+    freq_distribution = FreqDist(text)
+    return freq_distribution
 # TODO: Doc freq for each term
 # TODO: TF-IDF Calc
+def tf_idf():
+    pass
 # TODO: One more weight system
-
 # TODO: Analyze Queries
 # TODO: Calculate Precision 
 # TODO:           & Recall
 # TODO: Graph Precision & Recall
 
+
+""" Test samples """
+# sample = "This is a sentence of a bunch of words and more words and a butt plus second butt of a kind of thing and stuff love sentence."
+# words = reuters.raw(file_ids[0])
+
+documents = get_docs()
+term_per_doc = {}
+for doc_id in documents.keys():
+    term_per_doc[doc_id] = term_frequency(documents[doc_id])
+
+# print(term_per_doc.items()) # Test
